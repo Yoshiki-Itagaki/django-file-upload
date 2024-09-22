e6644c3f3ab98d67133938411df2bc6c1ea7dbc1
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 18:20:20 +0900

    Enabled use of session data

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from reviews.models import Review
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -43,6 +44,13 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
     
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
     
 class AddFavoriteView(View):
     def post(self, request):
